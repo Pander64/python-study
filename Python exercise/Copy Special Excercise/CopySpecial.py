d4798ef541cb029aca84db117d9ef06c1d986415
Alexander Barroso Gonzalez (CM) <abarroso@copaair.com>
2022-04-21 15:17:29 -0500

    Changes

--- Python exercise/Copy Special Excercise/CopySpecial.py
+++ Python exercise/Copy Special Excercise/CopySpecial.py
@@ -6,95 +6,87 @@
 # Google's Python Class
 # http://code.google.com/edu/languages/google-python-class/
 
-import cmd
-from importlib.resources import path
-import sys
-import re
-import os
-import shutil
-import subprocess
-from telnetlib import STATUS
+# Problem description:
+# https://developers.google.com/edu/python/exercises/copy-special
 
-"""Copy Special exercise
-"""
+from pathlib import Path
+from zipfile import ZipFile
+
+import shutil
+import sys
+
+
+"""Copy Special exercise"""
+
 
 # +++your code here+++
 # Write functions and modify main() to call them
+def get_special_paths(dir):
+    return [str(f) for f in Path(dir).resolve().iterdir()
+            if f.is_file() and f.match('*__*__*')]
 
+
+def copy_to(paths, dir):
+    dst = Path(dir)
+    dst.mkdir(parents=True, exist_ok=True)
+
+    for special_files in paths:
+        shutil.copy2(special_files, str(dst))
+
+
+def zip_to(paths, dir):
+    dst = Path(dir)
+    name_file = 'temp.zip'
+
+    if dst.name.endswith('.zip'):
+        name_file = dst.name
+        dst = dst.parents[0]
+
+    dst.mkdir(parents=True, exist_ok=True)
+    dst = dst / name_file
+
+    print(f"Command I'm going to do:zip -j {dst}")
+    with ZipFile(str(dst), 'w') as zip:
+        for special_files in paths:
+            print(f"{special_files}")
+            zip.write(special_files)
 
 
 def main():
-  # This basic command line argument parsing code is provided.
-  # Add code to call your functions below.
+    # This basic command line argument parsing code is provided.
+    # Add code to call your functions below.
 
-  # Make a list of command line arguments, omitting the [0] element
-  # which is the script itself.
-  args = sys.argv[1:]
-  if not args:
-    print ("usage: [--todir dir][--tozip zipfile] dir [dir ...]");
-    sys.exit(1)
+    # Make a list of command line arguments, omitting the [0] element
+    # which is the script itself.
+    args = sys.argv[1:]
+    if not args:
+        print("usage: [--todir dir][--tozip zipfile] dir [dir ...]")
+        sys.exit(1)
 
-  # todir and tozip are either set from command line
-  # or left as the empty string.
-  # The args array is left just containing the dirs.
-  todir = ''
-  if args[0] == '--todir':
-    todir = args[1]
-    del args[0:2]
+    # todir and tozip are either set from command line
+    # or left as the empty string.
+    # The args array is left just containing the dirs.
+    todir = ''
+    if args[0] == '--todir':
+        todir = args[1]
+        del args[0:2]
 
-  tozip = ''
-  if args[0] == '--tozip':
-    tozip = args[1]
-    del args[0:2]
+        copy_to(get_special_paths(args[0]), todir)
 
-  if len(args) == 0:
-    print ("error: must specify one or more dirs")
-    sys.exit(1)
+    tozip = ''
+    if args[0] == '--tozip':
+        tozip = args[1]
+        del args[0:2]
 
-  # +++your code here+++
+        zip_to(get_special_paths(args[0]), tozip)
 
-  # Gather all the special files
-  paths = []
-  for dirname in args:
-    paths.extend(get_special_path(dirname))
+    if len(args) == 0:
+        print("error: must specify one or more dirs")
+        sys.exit(1)
 
-  if todir:
-    copy_to(paths, todir)
-  elif tozip:
-    zip_to(paths, tozip)
-  else:
-    print('\n'.join(paths))
+        # +++your code here+++
+        # Call your functions
 
-  # Call your functions
-  def get_special_path(dirname):
-    """Given a dirname, returns a list of all its special files."""
-    result = []
-    paths = os.listdir(dirname) # list of path in that dir
-    for fname in paths:
-      match = re.search(r'__(\w+)__',fname)
-      if match:
-        result.append(os.path.abspath(os.path.join(dirname, fname)))
-    return result
-
-  def copy_to(paths, to_dir):
-    """Copy all of the given files to the given dir, creating it if necessary."""
-    if not os.path.exists(to_dir):
-      os.mkdir(to_dir)
-    for path in paths:
-      fname = os.path.basename(path)
-      shutil.copy(path, os.path.join(to_dir, fname))
-      # Could error out if alredy exists os.path.exists():
-
-  def zip_to(paths, zipfile):
-    """Zip up all of the given file into a new zip file with the given name."""
-    cmd = 'zip -j ' + zipfile + ' ' + ' '.join(path)
-    print("Command I'm going to do:" + cmd)
-    (status, output) = subprocess.getstatusoutput(cmd) 
-    # If command had a problem (status inn non-zero)
-    # print its output to stderr and exit
-    if status:
-      sys.stderr.write(output)
-      sys.exit(1)
 
 if __name__ == "__main__":
-  main()
+    main()
--- Python exercise/Log Puzzle Excercise/logpuzzle.py
+++ Python exercise/Log Puzzle Excercise/logpuzzle.py
@@ -6,97 +6,84 @@
 # Google's Python Class
 # http://code.google.com/edu/languages/google-python-class/
 
-from ast import If
-from importlib.resources import path
-import math
-import os
+from pathlib import Path
+
 import re
 import sys
-import urllib.request, urllib.parse, urllib.error
+import urllib.request
 
 """Logpuzzle exercise
 Given an apache logfile, find the puzzle urls and download the images.
-
 Here's what a puzzle url looks like:
-10.254.254.28 - - [06/Aug/2007:00:13:48 -0700] "GET /~foo/puzzle-bar-aaab.jpg HTTP/1.0" 302 528 "-" "Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.8.1.6) Gecko/20070725 Firefox/2.0.0.6"
 """
 
-def url_sort_key(url):
-  """Used to order the urls in increasing order by 2nd word if present"""
-  match = re.search(r'-(\w+)-(\w+)\.\w+', url)
-  if match:
-    return match.group(2)
-  else:
-    return url
 
 def read_urls(filename):
-  """Returns a list of the puzzle urls from the given log file,
-  extracting the hostname from the filename itself.
-  Screens out duplicate urls and returns the urls sorted into
-  increasing order."""
-  # +++your code here+++
-  # Extract the hosname if the filename
-  underbar = filename.index('_')
-  host = filename[underbar + 1:]
+    """Returns a list of the puzzle urls from the given log file,
+    extracting the hostname from the filename itself.
+    Screens out duplicate urls and returns the urls sorted into
+    increasing order."""
 
-  # Store the urls into a dict to screen out the duplicate 
-  url_dict = {}
+    hostname = 'http://' + Path(filename).name.split('_')[-1]
+    url = r'GET\s(.+/puzzle/.+\.jpg)'
 
-  f = open(filename)
-  for line in f:
-    # Find the path which is the after GET and surrounder by space.
-    match = re.search(r'"GET (\S+)', line)
-    # Above uses \S (upper case S) which is any not-space char 
-    # Alternative could use square brackets: "Get ([^ ])"
-    # or the ? form: "GET (.+?) "
-    
-    if 'puzzle' in path:
-      url_dict['http://' + host + path] = 1
+    with open(filename) as logfile:
+        lines = logfile.readlines()
 
-  return sorted(list(url_dict.keys()), key=url_sort_key)  
+    urls = [hostname + re.search(url, l).group(1)
+            for l in lines if re.search(url, l)]
+
+    return sorted(list(set(urls)), key=lambda s: s.split('-')[-1])
+
 
 def download_images(img_urls, dest_dir):
-  """Given the urls already in the correct order, downloads
-  each image into the given directory.
-  Gives the images local filenames img0, img1, and so on.
-  Creates an index.html in the directory
-  with an img tag to show each local image file.
-  Creates the directory if necessary.
-  """
-  # +++your code here+++
-  if not os.path.exists(dest_dir):
-    os.makedirs(dest_dir)
+    """Given the urls already in the correct order, downloads
+    each image into the given directory.
+    Gives the images local filenames img0, img1, and so on.
+    Creates an index.html in the directory
+    with an img tag to show each local image file.
+    Creates the directory if necessary.
+    """
+    dst = Path(dest_dir)
+    dst.mkdir(parents=True, exist_ok=True)
 
-  index = file(os.path.join(dest_dir, 'index.html'), 'w')
-  index.write('<html><body>\n')
+    imgs_dst = str(dst.resolve()) + '/img'
+    html_file = str(dst.resolve()) + '/index.html'
 
-  i = 0 
-  for img_url in img_urls:
-    local_name = 'img%d' % i 
-    print('Retrieving ...', img_url)
-    urllib.request.urlretrieve(img_url, os.path.join(dest_dir, local_name))
+    image_tags = []
 
-    index.write('\n</body></html>\n')
-    index.close()
+    for idx, url in enumerate(img_urls):
+        print(f'Downloading image {url}')
+        urllib.request.urlretrieve(url, f'{imgs_dst}{idx}.jpg')
+        image_tags.append(f'<img src="{imgs_dst}{idx}.jpg">')
+        print(f'Saved in {imgs_dst}{idx}.jpg')
+
+    html = f"<html>\n<body>\n{''.join(image_tags)}\n</body>\n</html>"
+
+    with open(html_file, 'w') as index:
+        index.write(html)
+        print('Created index.html to visualize images')
+
 
 def main():
-  args = sys.argv[1:]
+    args = sys.argv[1:]
 
-  if not args:
-    print ('usage: [--todir dir] logfile ')
-    sys.exit(1)
+    if not args:
+        print('usage: [--todir dir] logfile ')
+        sys.exit(1)
 
-  todir = ''
-  if args[0] == '--todir':
-    todir = args[1]
-    del args[0:2]
+    todir = ''
+    if args[0] == '--todir':
+        todir = args[1]
+        del args[0:2]
 
-  img_urls = read_urls(args[0])
+    img_urls = read_urls(args[0])
 
-  if todir:
-    download_images(img_urls, todir)
-  else:
-    print ('\n'.join(img_urls))
+    if todir:
+        download_images(img_urls, todir)
+    else:
+        print('\n'.join(img_urls))
+
 
 if __name__ == '__main__':
-  main()
+    main()
